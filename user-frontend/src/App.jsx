3a4f73808467990a0f0836045f66cce04fe0a881
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-19 21:23:49 +0200

    Added links and setup routes

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { PostContainer } from "./pages/PostContainer";
 import { SinglePost } from "./components/SinglePost";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
 
 export const App = () => {
   return (
@@ -12,6 +14,8 @@ export const App = () => {
         <Routes>
           <Route path='/' element={<PostContainer />} />
           <Route path='/post/:id' element={<SinglePost />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
         </Routes>
       </div>
     </BrowserRouter>
--- user-frontend/src/components/Navbar.jsx
+++ user-frontend/src/components/Navbar.jsx
@@ -30,22 +30,34 @@ export const Navbar = () => {
           )}
         </div>
         <div className='hidden md:flex gap-4 text-lg font-medium '>
-          <button className='text-neutral-600 hover:text-neutral-900'>
+          <Link
+            to={"/signup"}
+            className='text-neutral-600 hover:text-neutral-900'
+          >
             Sign Up
-          </button>
-          <button className='text-neutral-600 hover:text-neutral-900'>
+          </Link>
+          <Link
+            to={"/login"}
+            className='text-neutral-600 hover:text-neutral-900'
+          >
             Login
-          </button>
+          </Link>
         </div>
       </div>
       {openMenu && (
         <div className='w-full h-fit flex flex-col bg-neutral-50 p-8 gap-3 md:hidden'>
-          <button className='w-full py-2 border-y border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'>
+          <Link
+            to={"/signup"}
+            className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
+          >
             Sign Up
-          </button>
-          <button className='w-full py-2 border-y border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'>
+          </Link>
+          <Link
+            to={"/login"}
+            className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
+          >
             Login
-          </button>
+          </Link>
         </div>
       )}
     </div>
