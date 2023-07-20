2c9eaab7e26b8e0ccef5a7fd95139321bd2357ff
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 15:23:48 +0200

    Fixed single post image size

--- user-frontend/src/components/SinglePost.jsx
+++ user-frontend/src/components/SinglePost.jsx
@@ -9,7 +9,7 @@ export const SinglePost = () => {
   return (
     <div className='p-6 md:px-16 lg:px-24 w-full flex justify-center'>
       <div className='w-full h-fit flex flex-col gap-5 items-center md:w-3/4'>
-        <img src={post.image} className='rounded-lg shadow-xl' />
+        <img src={post.image} className='rounded-lg shadow-xl md:h-80' />
         <h1 className='text-center font-bold text-xl place-self-start'>
           {post.title}
         </h1>
