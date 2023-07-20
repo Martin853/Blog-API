826a1fc427b8543a0ba8e3c4c299aa0f85c27be6
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 18:49:00 +0200

    Added salted hashes and validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,38 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// Static Signup Method
+
+userSchema.statics.signup = async function (email, password) {
+  // Validation
+
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(`The password must contain a minimum of 8 characters.
+It must include at least 1 alphabet (a-z or A-Z).
+It must include at least 1 number (0-9).`);
+  }
+
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("Email already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
