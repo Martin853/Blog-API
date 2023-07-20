a1f3090cee05121cff9bc52e2cc485a39ee706e7
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 21:07:25 +0200

    Updated login and signup to have states for username and password

--- user-frontend/src/pages/Login.jsx
+++ user-frontend/src/pages/Login.jsx
@@ -4,22 +4,41 @@ import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 export const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState("password");
 
+  // Variables
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Handle Submit
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
     <div className='p-6 md:px-16 lg:px-24 w-full h-screen flex justify-center items-center'>
-      <form className='w-full h-fit p-6 bg-neutral-100 rounded-md shadow-lg flex flex-col gap-4 md:w-5/6 md:text-xl'>
+      <form
+        className='w-full h-fit p-6 md:py-12 bg-neutral-100 rounded-md shadow-lg flex flex-col gap-4 md:w-5/6 md:text-xl'
+        onSubmit={handleSubmit}
+      >
         <h1 className='text-lg md:text-2xl'>Login</h1>
 
         <label>Email</label>
         <input
           type='text'
+          value={email}
           className=' w-full rounded p-2 shadow-md outline-none'
+          onChange={(event) => setEmail(event.target.value)}
         />
 
         <label>Password</label>
         <div className='w-full h-fit p-2 flex bg-white rounded shadow-md items-center'>
           <input
             type={passwordVisible}
+            value={password}
             className=' w-full outline-none'
+            onChange={(event) => setPassword(event.target.value)}
           ></input>
           {passwordVisible == "password" ? (
             <AiFillEye
