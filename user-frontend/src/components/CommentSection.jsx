601001d941d820e071a6e43a4cfbaae98459395d
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 16:15:29 +0200

    Added comment section

---
+++ user-frontend/src/components/CommentSection.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
+export const CommentSection = ({ post }) => {
+  const [comments, setComments] = useState([]);
+  const id = post._id;
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/posts/${id}/comments`
+      );
+      const json = await response.json();
+
+      if (response.ok) {
+        setComments(json);
+      }
+    };
+
+    fetchComments();
+  }, []);
+
+  return (
+    <div className='w-full h-fit md:w-3/4 flex flex-col items-start gap-3'>
+      <h1 className='font-bold text-xl'>Comments</h1>
+      <div className='w-full flex flex-col gap-3'>
+        {comments.length > 0 ? (
+          comments.map((comment) => {
+            return (
+              <div
+                key={comment._id}
+                className='w-full h-fit p-4 bg-neutral-50 rounded-md shadow-lg shadow-neutral-300 text-lg flex flex-col gap-3'
+              >
+                <div className='flex gap-3 items-center'>
+                  <h1 className='font-bold'>User</h1>
+                  <h1 className='text-base text-gray-600'>
+                    {formatDistanceToNow(new Date(comment.createdAt), {
+                      addSuffix: true,
+                    })}
+                  </h1>
+                </div>
+                <p>{comment.content}</p>
+              </div>
+            );
+          })
+        ) : (
+          <div className='w-full p-6 text-center mt-2 text-lg'>
+            There are no comments for this post
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- user-frontend/src/components/SinglePost.jsx
+++ user-frontend/src/components/SinglePost.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { CommentSection } from "./CommentSection";
 
 export const SinglePost = () => {
   const location = useLocation();
   const { post } = location.state;
-  console.log(post);
 
   return (
-    <div className='p-6 md:px-16 lg:px-24 w-full flex justify-center'>
+    <div className='p-6 md:px-16 lg:px-24 w-full flex flex-col items-center justify-center gap-8'>
       <div className='w-full h-fit flex flex-col gap-5 items-center md:w-3/4'>
         <img src={post.image} className='rounded-lg shadow-xl md:h-80' />
         <h1 className='text-center font-bold text-xl place-self-start'>
@@ -15,6 +15,7 @@ export const SinglePost = () => {
         </h1>
         <p>{post.description}</p>
       </div>
+      <CommentSection post={post} />
     </div>
   );
 };
