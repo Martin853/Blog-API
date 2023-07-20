eeca13f88722603bf94b62c2dfd258c6e44218a2
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 15:17:16 +0200

    Added loading screen to post container

--- user-frontend/src/pages/PostContainer.jsx
+++ user-frontend/src/pages/PostContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { PostCard } from "../components/PostCard";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const PostContainer = () => {
   const [posts, setPosts] = useState(null);
@@ -16,6 +17,20 @@ export const PostContainer = () => {
 
     fetchPosts();
   }, []);
+
+  if (!posts) {
+    return (
+      <div className='p-6 md:px-16 lg:px-24 flex flex-col w-full h-screen gap-6'>
+        <h1 className='text-lg'>Posts</h1>
+        <div className='w-full h-full flex justify-center items-center'>
+          <div className='flex flex-col items-center gap-4'>
+            <h1 className='text-3xl font-medium'>Loading...</h1>
+            <AiOutlineLoading3Quarters className='text-4xl animate-spin' />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='p-6 md:px-16 lg:px-24 flex flex-col w-full h-fit gap-6'>
