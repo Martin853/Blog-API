f0b92a8d0bcf8cba5fcee51f66c776e253b9453d
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-20 20:39:44 +0200

    Added redux and configured user state

--- user-frontend/src/main.jsx
+++ user-frontend/src/main.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
+import configureStore from "./redux/configureStore.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={configureStore}>
+    <App />
+  </Provider>
+);
