684a8451997b5b742813d438f5f12f4fe0f470da
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-21 16:25:19 +0200

    Set a initial state for the user in app.js

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { PostContainer } from "./pages/PostContainer";
 import { SinglePost } from "./components/SinglePost";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
+import { useDispatch } from "react-redux";
+import { login } from "./redux/userSlice";
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      dispatch(login(user));
+    }
+  });
+
   return (
     <BrowserRouter>
       <div className='font-inter'>
