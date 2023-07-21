d73f1c60028bd121d15fa1f24239a1340ae1fb4d
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-21 19:26:31 +0200

    Added routes protection

--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { PostContainer } from "./pages/PostContainer";
 import { SinglePost } from "./components/SinglePost";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "./redux/userSlice";
 
 export const App = () => {
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user.value);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
@@ -17,7 +19,7 @@ export const App = () => {
     if (user) {
       dispatch(login(user));
     }
-  });
+  }, []);
 
   return (
     <BrowserRouter>
@@ -26,8 +28,14 @@ export const App = () => {
         <Routes>
           <Route path='/' element={<PostContainer />} />
           <Route path='/post/:id' element={<SinglePost />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
+          <Route
+            path='/login'
+            element={!user ? <Login /> : <Navigate to='/' />}
+          />
+          <Route
+            path='/signup'
+            element={!user ? <SignUp /> : <Navigate to='/' />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
