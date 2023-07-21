337d4ed15fc349046b4779814cb1754fa3649695
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-21 16:15:56 +0200

    Conditionally rendered logout, email, signup,login

--- user-frontend/src/components/Navbar.jsx
+++ user-frontend/src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useSelector } from "react-redux";
 
 export const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const { logout } = useLogout();
+  const user = useSelector((state) => state.user.value);
 
   return (
     <div className='w-full'>
@@ -31,47 +33,68 @@ export const Navbar = () => {
             />
           )}
         </div>
-        <div className='hidden md:flex gap-4 text-lg font-medium '>
-          <button
-            className='text-neutral-600 hover:text-neutral-900'
-            onClick={() => logout()}
-          >
-            Log out
-          </button>
-          <Link
-            to={"/signup"}
-            className='text-neutral-600 hover:text-neutral-900'
-          >
-            Sign Up
-          </Link>
-          <Link
-            to={"/login"}
-            className='text-neutral-600 hover:text-neutral-900'
-          >
-            Login
-          </Link>
+        <div className='hidden md:flex gap-4 text-lg font-medium items-center'>
+          {user && (
+            <div className='flex gap-3 items-center'>
+              <span className='text-base'>{user.email}</span>
+              <button
+                className='text-neutral-600 hover:text-neutral-900'
+                onClick={() => logout()}
+              >
+                Log out
+              </button>
+            </div>
+          )}
+          {!user && (
+            <div className='flex gap-3 items-center'>
+              <Link
+                to={"/signup"}
+                className='text-neutral-600 hover:text-neutral-900'
+              >
+                Sign Up
+              </Link>
+              <Link
+                to={"/login"}
+                className='text-neutral-600 hover:text-neutral-900'
+              >
+                Login
+              </Link>
+            </div>
+          )}
         </div>
       </div>
       {openMenu && (
         <div className='w-full h-fit flex flex-col bg-neutral-50 p-8 gap-3 md:hidden'>
-          <button
-            className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
-            onClick={() => logout()}
-          >
-            Log out
-          </button>
-          <Link
-            to={"/signup"}
-            className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
-          >
-            Sign Up
-          </Link>
-          <Link
-            to={"/login"}
-            className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
-          >
-            Login
-          </Link>
+          {user && (
+            <div className='w-full flex flex-col gap-3'>
+              <span className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'>
+                {user.email}
+              </span>
+              <button
+                className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
+                onClick={() => logout()}
+              >
+                Log out
+              </button>
+            </div>
+          )}
+
+          {!user && (
+            <div className='w-full flex flex-col gap-3'>
+              <Link
+                to={"/signup"}
+                className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
+              >
+                Sign Up
+              </Link>
+              <Link
+                to={"/login"}
+                className='w-full py-2 border-y text-center border-y-neutral-200 hover:bg-neutral-100 transition-all duration-300 ease-linear'
+              >
+                Login
+              </Link>
+            </div>
+          )}
         </div>
       )}
     </div>
