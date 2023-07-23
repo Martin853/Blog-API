5281b16cbc5658a3319a60b1b9744c37096c4a8f
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-23 20:08:22 +0200

    Set up the app component

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const App = () => {
-  return <div className='font-inter'>App</div>;
+  return (
+    <div className='font-inter w-full px-4 sm:px-12 md:px-24 lg:px-32'></div>
+  );
 };
