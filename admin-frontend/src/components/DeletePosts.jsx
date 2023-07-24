f7e670b92fc2eaa95b054a23a13a96d78b35aa8c
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-24 18:09:28 +0200

    Created delete posts design and changed button, text design

--- admin-frontend/src/components/DeletePosts.jsx
+++ admin-frontend/src/components/DeletePosts.jsx
@@ -41,14 +41,14 @@ export const DeletePosts = () => {
               className='w-full h-full flex flex-col justify-between gap-3 items-center p-4 flex-grow'
             >
               <img src={post.image} className='rounded' />
-              <h1 className='text-sm font-semibold text-center'>
+              <h1 className='text-sm md:text-lg font-semibold text-center'>
                 {post.title}
               </h1>
               <button
                 onClick={() => {
                   deletePost(post._id);
                 }}
-                className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+                className='w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
               >
                 Delete
               </button>
