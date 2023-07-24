343aeb983b0165a9725dfd67eeacec90c9e7358c
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-24 17:00:02 +0200

    Created the add post component design

--- admin-frontend/src/components/AddPost.jsx
+++ admin-frontend/src/components/AddPost.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export const AddPost = () => {
-  return <div>AddPost</div>;
+  return (
+    <div className='w-full flex flex-col items-center gap-3'>
+      <h1 className='text-xl font-semibold'>Add Post</h1>
+      <form className='w-full flex flex-col gap-5'>
+        <label>Post Title</label>
+        <input
+          type='text'
+          className='border border-neutral-200 rounded-md p-1 outline-none focus:border-indigo-700'
+        />
+
+        <label>Post Content</label>
+        <textarea
+          className='border border-neutral-200 rounded-md p-1 outline-none focus:border-indigo-700'
+          rows={4}
+        />
+
+        <label>Post Image (URL)</label>
+        <input
+          type='url'
+          className='border border-neutral-200 rounded-md p-1 outline-none focus:border-indigo-700'
+        />
+
+        <button className='w-full p-2 border border-black rounded-md hover:bg-black hover:text-white transition-all duration-300 ease-in-out'>
+          Create
+        </button>
+      </form>
+    </div>
+  );
 };
